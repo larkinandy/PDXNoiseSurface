98a5b87cd26c10cd31316128111d52b93c73f405
Andrew Larkin <larkinan@oregonstate.edu>
2024-07-20 12:58:47 -0700

    Update createRasters.py

--- CreateRasterSurface/createRasters.py
+++ CreateRasterSurface/createRasters.py
@@ -13,14 +13,15 @@ HOME_FOLDER = "H:/Noise/"
 PREDICTION_FOLDER = HOME_FOLDER + "implementation/predictions/"
 TEMP_FOLDER = PREDICTION_FOLDER + "tempFiles.gdb/"
 LEQ_POINT_FILE = PREDICTION_FOLDER + "LEQ.csv"
-LEQ_SHAPEFILE = TEMP_FOLDER + "LEQ"
+LEQ_SHAPEFILE = TEMP_FOLDER + "LEQScreened4"
 DNL_POINT_FILE = PREDICTION_FOLDER + "DNL.csv"
 DNL_SHAPEFILE = TEMP_FOLDER + "DNL"
-LEQ_RASTER_FILE = PREDICTION_FOLDER + "LEQ.tif"
+LEQ_RASTER_FILE = PREDICTION_FOLDER + "LEQScreened2.tif"
 DNL_RASTER_FILE = PREDICTION_FOLDER + "DNL.tif"
 BUILDINGS_SHAPEFILE = HOME_FOLDER + "building/buildingMergedDissolve2/buildingMergedDissolve2.shp"
-WATER_SHAPEFILE = HOME_FOLDER + "roads/oregon-latest-free.shp/gis_osm_water_a_free_1.shp"
-CITY_BOUNDARY = HOME_FOLDER + "CityBoundary/Portland_City_Boundary/Portland_City_Boundary.shp"
+WATER_SHAPEFILE = "H:/Noise/implementation/Willamette_Columbia_High_Water/Willamette_Columbia_River_Ordinary_High_Water.shp"
+CITY_BOUNDARY = HOME_FOLDER + "CityBoundary/Portland_City_Boundary/PDX_Boundary.shp"
+AIRPORT_SHAPEFILE = "C:/users/larki/downloads/PDX_Boundary/PDX_Boundary.shp"
 
 # CRS that lat/long coords are stored in
 CRS_SHAPEFILE = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]];-400 -400 1000000000;-100000 10000;-100000 10000;8.98315284119521E-09;0.001;0.001;IsHighPrecision'
@@ -47,7 +48,6 @@ def createShapefile(inCSV,outFilepath):
         z_field=None,
         coordinate_system=CRS_SHAPEFILE
     )
-
     # remove points that are within water bodies
     selectedFeatures = arcpy.management.SelectLayerByLocation(
         in_layer=tempShp,
@@ -99,39 +99,66 @@ def makeRaster(inShapefile,valueField,outputRaster):
             build_rat="BUILD"
         )
 
+
     # interpolate to fill in building footprints
     output = arcpy.sa.Con(arcpy.sa.IsNull(rasterFilepath), 
-                 arcpy.sa.FocalStatistics(rasterFilepath,arcpy.sa.NbrRectangle(2,2, "CELL"),"MEAN"),
+                 arcpy.sa.FocalStatistics(rasterFilepath,arcpy.sa.NbrCircle(2, "CELL"),"MEAN",'NODATA'),
                  rasterFilepath)
-    
+
     # clamp maximum values t
     output = arcpy.sa.Con(output > 86, 86,output)
     output = arcpy.sa.Con(output < 44, 44,output)
+
+
     for x in range(3):
+        print(x)
         output = arcpy.sa.Con(arcpy.sa.IsNull(output), 
-                 arcpy.sa.FocalStatistics(output,arcpy.sa.NbrRectangle(2,2, "CELL"),"MEAN"),
+                 arcpy.sa.FocalStatistics(output,arcpy.sa.NbrCircle(2, "CELL"),"MEAN",'DATA'),
                  output)
         
-    
-        
-    # remove pixels that are in water bodies
-    output = arcpy.sa.ExtractByMask(
-        in_raster=output,
-        in_mask_data=WATER_SHAPEFILE,
-        extraction_area="OUTSIDE",
-        analysis_extent='#'
-    )
+        # remove pixels that are outside city boundaries
+        output = arcpy.sa.ExtractByMask(
+            in_raster=output,
+            in_mask_data=CITY_BOUNDARY,
+            extraction_area="INSIDE",
+            analysis_extent='#'
+        )
 
-    # remove pixels that are outside city boundaries
-    output = arcpy.sa.ExtractByMask(
-        in_raster=output,
-        in_mask_data=CITY_BOUNDARY,
-        extraction_area="INSIDE",
-        analysis_extent='#'
+        # remove pixels that are in water bodies
+        output = arcpy.sa.ExtractByMask(
+            in_raster=output,
+            in_mask_data=WATER_SHAPEFILE,
+            extraction_area="OUTSIDE",
+            analysis_extent='#'
     )
+    interp = 3
+    for x in range(24):
+        print(x)
+        output = arcpy.sa.Con(arcpy.sa.IsNull(output), 
+                 arcpy.sa.FocalStatistics(output,arcpy.sa.NbrCircle(interp + x, "CELL"),"MEAN",'DATA'),
+                 output)
+        
+        # remove pixels that are outside city boundaries
+        output = arcpy.sa.ExtractByMask(
+            in_raster=output,
+            in_mask_data=CITY_BOUNDARY,
+            extraction_area="INSIDE",
+            analysis_extent='#'
+        )
+
+        output = arcpy.sa.ExtractByMask(
+            in_raster=output,
+            in_mask_data=WATER_SHAPEFILE,
+            extraction_area="OUTSIDE",
+            analysis_extent='#'
+        )
+        
+    print("clipped to city boundaries")
 
     # convert raster from float to int to reduce file size
     output = arcpy.sa.Int(arcpy.sa.SetNull(output,output,"VALUE<0"))
+
+    print("converted to integer")
 
     # save raster
     output.save(outputRaster)
